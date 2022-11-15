e4adfac82c151b910f2402181eceeace5165f551
EnotelloR <uncleegor05@gmail.com>
2022-11-15 17:53:15 +0300

    Уменьшил размер рекомендации, добавил ссылку

--- src/features/animals/AnimalRecommendations/AnimalRecommendations.tsx
+++ src/features/animals/AnimalRecommendations/AnimalRecommendations.tsx
@@ -2,6 +2,8 @@ import React, { FC } from 'react';
 import { useGetAnimalRecommendationsQuery } from '../animals.service';
 import { Alert, Typography } from 'antd';
 import styles from './AnimalRecommendations.module.css';
+import { routes } from '../../../routes/routes';
+import { useNavigate } from 'react-router-dom';
 
 const { Title, Text } = Typography;
 
@@ -12,19 +14,28 @@ export const AnimalRecommendations: FC<{ pet_id: number }> = ({ pet_id }) => {
     isSuccess,
   } = useGetAnimalRecommendationsQuery(pet_id);
 
+  const navigate = useNavigate();
+
   return (
     <div className={styles.recommendationWrapper}>
       {isLoading && 'Данные загружаются...'}
       {isSuccess &&
         recommendations.result.map((recommendation) => {
           return (
-            <div key={recommendation.request_id} className={styles.recommendationCard}>
+            <div
+              key={recommendation.request_id}
+              className={styles.recommendationCard}
+              onClick={() => navigate(routes.navAppointment(recommendation.request_id))}
+              onKeyDown={() => navigate(routes.navAppointment(recommendation.request_id))}
+            >
               <div>
-                <Title level={2}>{recommendation.carrier_name}</Title>
+                <Title className={styles.recommendationText} level={2}>
+                  {recommendation.carrier_name}
+                </Title>
                 <Text>Дата: {recommendation.datetime || 'Не указана'}</Text>
               </div>
               <div>
-                <Title level={3}>Рекомендация: </Title>
+                <Title level={5}>Рекомендация: </Title>
                 <p className={styles.recommendationText}>
                   {recommendation.result_description || 'Не указана'}
                 </p>
